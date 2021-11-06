a1780ed82ae7be437b80659006b905ca436c3f47
aakash ranglani <ranglaniaakash.git@gmail.com>
2021-11-06 10:24:58 +0000

    code to fetch list of tags from docs

---
+++ src/main/java/com/tcd/lucene/Constants.java
@@ -0,0 +1,40 @@
+package com.tcd.lucene;
+
+public interface Constants {
+
+	interface FBIS {
+		final static String DOC = "DOC";
+		final static String DOC_NO = "DOCNO";
+		final static String HT = "HT";
+		final static String HEADER = "HEADER";
+		final static String H2 = "H2";
+		final static String DATE_1 = "DATE1";
+		final static String H3 = "H3";
+		final static String TI = "TI";
+		final static String TEXT = "TEXT";
+	}
+
+	interface FR94 {
+		final static String DOC = "DOC";
+		final static String DOC_NO = "DOCNO";
+		final static String PARENT = "PARENT";
+		final static String TEXT = "TEXT";
+		final static String USDEPT = "USDEPT";
+		final static String USBUREAU = "USBUREAU";
+		final static String CFRNO = "CFRNO";
+		final static String RINDOCK = "RINDOCK";
+		final static String AGENCY = "AGENCY";
+		final static String ACTION = "ACTION";
+		final static String SUMMARY = "SUMMARY";
+		final static String DATE = "DATE";
+		final static String ADDRESS = "ADDRESS";
+		final static String FURTHER = "FURTHER";
+		final static String SUPPLEM = "SUPPLEM";
+		final static String SIGNER = "SIGNER";
+		final static String SIGNJOB = "SIGNJOB";
+		final static String FRFILING = "FRFILING";
+		final static String BILLING = "BILLING";
+		final static String TABLE = "TABLE";
+	}
+
+}
