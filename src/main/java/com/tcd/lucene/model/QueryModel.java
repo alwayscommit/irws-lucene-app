b76dcd57da1d7a793b84fbcaec7455ec49fe2cd1
Shubham Uniyal <shubham.uniyal@gmail.com>
2021-11-11 22:58:31 +0000

    added model and parser files

---
+++ src/main/java/com/tcd/lucene/model/QueryModel.java
@@ -0,0 +1,42 @@
+package com.tcd.lucene.model;
+
+public class QueryModel {
+	private String queryNumber;
+	private String queryTitle;
+	private String description;
+	private String narrative;
+	
+	public String getQueryNumber() {
+		return queryNumber;
+	}
+	
+	public void setQueryNumber(String queryNumber) {
+		this.queryNumber = queryNumber;
+	}
+	
+	public String getQueryTitle() {
+		return queryTitle;
+	}
+	
+	public void setQueryTitle(String queryTitle) {
+		this.queryTitle = queryTitle;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		String actualDescription = description.replaceAll("Description: ", "");
+		this.description = actualDescription.trim();
+	}
+	
+	public String getNarrative() {
+		return narrative;
+	}
+	
+	public void setNarrative(String narrative) {
+		String actualNarrative = narrative.replaceAll("Narrative: ", "");
+		this.narrative = actualNarrative.trim();
+	}
+}
